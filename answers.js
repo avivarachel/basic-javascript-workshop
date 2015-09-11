964f36c8a94acb8d950f4b58d4e7ee1df8f074c3
Aviva Wolpert <wolpertaviva@gmail.com>
2015-09-11 19:59:59 +0000

    function that does verious operations with 2 numbers depending on string

--- answers.js
+++ answers.js
@@ -1,7 +1,7 @@
 // This function takes a string and returns the first character of the string
 
-function firstCharacter (letters) { // created function
-    return (letters)[0];
+function firstCharacter (string) { // created function
+    return (string)[0];
 }
 firstCharacter("Aviva"); //this will display the first letter "A"
 
@@ -37,7 +37,7 @@ function addingTwoNumbers (num1, num2) {
   return num1 + num2;   
 }
 
-addingTwoNumber (1, 2); /// this returns 3
+addingTwoNumbers (1, 2); /// this returns 3
 addingTwoNumbers (12, 1.5); /// this returns 13.5
 addingTwoNumbers ("pickles", 75); /// this will return "pickles75"
 
@@ -49,4 +49,30 @@ function multiplyingTwoNumbers (num1, num2) {
 multiplyingTwoNumbers(47, 567345); /// this will return 26665215
 multiplyingTwoNumbers(1, 1.5); /// this will return 1.5
 multiplyingTwoNumbers("piglets", 75); /// this will return NaN (not a number)
+
+/// function that takes two numbers and a string, returns various results
+
+function numNumString (num1, num2, string) {
+ if (string === "add")  {
+     return (num1 + num2);
+ }
+    else if (string === "subtract") {
+        return (num1 - num2);
+    }
+    else if (string === "mult") {
+        return (num1 * num2);
+    } 
+    else if (string === "div") {
+        return (num1 / num2);
+    }
+        else {
+            return 0;
+        }
+};
+
+numNumString(3, 4, "add"); // this will return 7
+numNumString(27, 65, "subtract"); // this will return -38
+numNumString(5, 6, "mult"); // this will return 30
+numNumString(5, 6, "div"); // this will return 0.8333333333333334
+numNumString(1, 2, "koala"); // this will return 0
 
