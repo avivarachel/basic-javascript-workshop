20f6ac863e724f7c17314a65ec9030199e5ce5d8
Aviva Wolpert <wolpertaviva@gmail.com>
2015-09-11 18:23:19 +0000

    added comments and further tests

--- answers.js
+++ answers.js
@@ -1,6 +1,13 @@
-function firstCharacter (letters) {
+// This function takes a string and returns the first character of the string
+
+function firstCharacter (letters) { // created function
     return (letters)[0];
 }
-firstCharacter("Aviva");
+firstCharacter("Aviva"); //this will display the first letter "A"
+
+firstCharacter("Hi"); // this will display "H"
+
+firstCharacter(" "); // this will display an empty space " "
+
 
 
