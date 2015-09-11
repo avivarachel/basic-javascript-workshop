47b106c4952c6daa01041c9ae582cb83a19007a1
Aviva Wolpert <wolpertaviva@gmail.com>
2015-09-11 18:41:01 +0000

    function with last letter of string

--- answers.js
+++ answers.js
@@ -9,5 +9,16 @@ firstCharacter("Hi"); // this will display "H"
 
 firstCharacter(" "); // this will display an empty space " "
 
+function lastCharacter (string) {
+    return string[string.length-1]
+}
+
+lastCharacter("Pamplemousse"); //this will return the last letter "e"
+
+lastCharacter("I am learning about functions"); // this will return "s"
+
+lastCharacter(" "); // this will return an empty string " "
+
+
 
 
